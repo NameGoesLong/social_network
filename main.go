ebdd6bce025704f1a2ecfee57151a16c9001d48d
ZTMDKY <zhangcheny99@gmail.com>
2021-04-18 22:53:32 +0000

    test handler

--- main.go
+++ main.go
@@ -1,8 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
-    fmt.Println("hello world")
-}
+	fmt.Println("started-service")
+	http.HandleFunc("/upload", uploadHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 
+}
