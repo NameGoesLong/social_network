c78770927b81b01d2d19127886afb866ef51e024
ZTMDKY <zhangcheny99@gmail.com>
2021-04-23 04:12:45 +0000

    implement the search functionality

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ func main() {
 	fmt.Println("started-service")
 	r := mux.NewRouter()
 	r.Handle("/upload", http.HandlerFunc(uploadHandler)).Methods("POST", "OPTIONS")
+	r.Handle("/search", http.HandlerFunc(searchHandler)).Methods("GET", "OPTIONS")
 	log.Fatal(http.ListenAndServe(":8080", r))
 
 }
