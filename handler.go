688683de490f6c67ced822d1aff7aa716c6047b4
ZTMDKY <zhangcheny99@gmail.com>
2021-04-24 18:34:36 +0000

    finished configuring ECS

--- handler.go
+++ handler.go
@@ -4,11 +4,29 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"path/filepath"
+
+	"github.com/pborman/uuid"
+)
+
+var (
+	mediaTypes = map[string]string{
+		".jpeg": "image",
+		".jpg":  "image",
+		".gif":  "image",
+		".png":  "image",
+		".mov":  "video",
+		".mp4":  "video",
+		".avi":  "video",
+		".flv":  "video",
+		".wmv":  "video",
+	}
 )
 
 func uploadHandler(w http.ResponseWriter, r *http.Request) {
 	// Parse from body of request to get a json object.
-	fmt.Println("Received one post request")
+	fmt.Println("Received one upload request")
+
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Authorization")
 
@@ -16,29 +34,46 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	decoder := json.NewDecoder(r.Body)
-	var p Post
-	if err := decoder.Decode(&p); err != nil {
-		panic(err)
+	p := Post{
+		Id:      uuid.New(),
+		User:    r.FormValue("user"),
+		Message: r.FormValue("message"),
 	}
 
-	fmt.Fprintf(w, "Post received: %s\n", p.Message)
+	file, header, err := r.FormFile("media_file")
+	if err != nil {
+		http.Error(w, "Media file is not available", http.StatusBadRequest)
+		fmt.Printf("Media file is not available %v\n", err)
+		return
+	}
+
+	suffix := filepath.Ext(header.Filename)
+	if t, ok := mediaTypes[suffix]; ok {
+		p.Type = t
+	} else {
+		p.Type = "unknown"
+	}
+
+	err = savePost(&p, file)
+	if err != nil {
+		http.Error(w, "Failed to save post to GCS or Elasticsearch", http.StatusInternalServerError)
+		fmt.Printf("Failed to save post to GCS or Elasticsearch %v\n", err)
+		return
+	}
+
+	fmt.Println("Post is saved successfully.")
 }
 
 func searchHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Received one request for search")
-
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Authorization")
 	w.Header().Set("Content-Type", "application/json")
-
 	if r.Method == "OPTIONS" {
 		return
 	}
-
 	user := r.URL.Query().Get("user")
 	keywords := r.URL.Query().Get("keywords")
-
 	var posts []Post
 	var err error
 	if user != "" {
@@ -46,13 +81,11 @@ func searchHandler(w http.ResponseWriter, r *http.Request) {
 	} else {
 		posts, err = searchPostsByKeywords(keywords)
 	}
-
 	if err != nil {
 		http.Error(w, "Failed to read post from Elasticsearch", http.StatusInternalServerError)
 		fmt.Printf("Failed to read post from Elasticsearch %v.\n", err)
 		return
 	}
-
 	js, err := json.Marshal(posts)
 	if err != nil {
 		http.Error(w, "Failed to parse posts into JSON format", http.StatusInternalServerError)
